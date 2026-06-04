ad4a4b63d1926817173fb005c1fa1b8e14c5de40
nphcuong <nphcuong205@gmail.com>
2026-06-04 10:57:44 +0700

    test: fix integration tests

--- backend/src/test/java/com/medical/system/repository/ServiceRequestRepositoryTest.java
+++ backend/src/test/java/com/medical/system/repository/ServiceRequestRepositoryTest.java
@@ -3,8 +3,12 @@ package com.medical.system.repository;
 import com.medical.system.AbstractIntegrationTest;
 import com.medical.system.model.entity.Asset;
 import com.medical.system.model.entity.ServiceRequest;
+import com.medical.system.model.entity.User;
 import com.medical.system.model.enums.AssetStatus;
 import com.medical.system.model.enums.RequestStatus;
+import com.medical.system.repository.AssetRepository;
+import com.medical.system.repository.ServiceRequestRepository;
+import com.medical.system.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,9 +26,18 @@ class ServiceRequestRepositoryTest extends AbstractIntegrationTest {
     @Autowired
     private AssetRepository assetRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void findByAssetIdAndStatusInOrderByCreatedAtDesc_shouldReturnMatchedRequests() {
         // Arrange
+        User user = new User();
+        user.setUsername("doctor_test");
+        user.setPassword("pass");
+        user.setRole(com.medical.system.model.enums.Role.DOCTOR);
+        user = userRepository.save(user);
+
         Asset asset = Asset.builder()
                 .code("CT-001")
                 .name("CT Scanner")
@@ -34,12 +47,14 @@ class ServiceRequestRepositoryTest extends AbstractIntegrationTest {
 
         ServiceRequest pendingReq = ServiceRequest.builder()
                 .asset(asset)
+                .reportedBy(user)
                 .status(RequestStatus.PENDING)
                 .description("Pending issue")
                 .build();
         
         ServiceRequest completedReq = ServiceRequest.builder()
                 .asset(asset)
+                .reportedBy(user)
                 .status(RequestStatus.COMPLETED)
                 .description("Completed issue")
                 .build();
